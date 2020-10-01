b3838c7159a79a1807e9561a87b5aa1f1b7e23c6
Mithun Biswas <bhoot.biswas@gmail.com>
2020-10-01 16:45:06 +0600

    Add initial files

---
+++ job-salary.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Job Salary
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     job-salary
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Job_Salary
+ */
+
+// Your code starts here.
